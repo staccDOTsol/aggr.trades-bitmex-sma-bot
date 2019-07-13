ff77eaf3e9fc41ea3eb6ede826adff94ea2eb3d5
Your Name <you@example.com>
2019-07-13 15:55:46 -0300

    b y

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -601,8 +601,9 @@ this.chart.series[7].data[a].remove();
     }
 
     if (
-    this.chart.series[1].data[this.chart.series[1].data.length-1] != undefined){
-    console.log(this.chart.series[1].data[this.chart.series[1].data.length-1].plotY)
+    this.chart.series[1].data[this.chart.series[5].data.length-1] != undefined){
+    console.log(this.chart.series[5].data[this.chart.series[4].data.length-1].plotY)
+    console.log(this.chart.series[5].data[this.chart.series[5].data.length-1].plotY)
     }
     if(this.chart.series[5].data[this.chart.series[5].data.length-1] != undefined){
 
