2be4745f5168759f11c30e943111d36b01b0c41c
RegTheIII <jarettrsdunn@gmail.com>
2019-07-16 18:58:11 -0300

    iteration...

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -1334,7 +1334,7 @@ request(requestOptions, function(error, response, body) {
          verb = 'DELETE',
   path = '/api/v1/order/all',
   expires = Math.round(new Date().getTime() / 1000) + 6660, // 1 min in the future
-  data = {filter:{"pegPriceType":""},symbol:thepair.replace('BTCUSD','XBTUSD').replace('BTC','U19')};
+  data = {filter:{"ordType":"Limit"},symbol:thepair.replace('BTCUSD','XBTUSD').replace('BTC','U19')};
 
 // Pre-compute the postBody so we can be sure that we're using *exactly* the same body in the request
 // and in the signature. If you don't do this, you might get differently-sorted keys and blow the signature.
@@ -1869,7 +1869,7 @@ request(requestOptions, function(error, response, body) {
         verb = 'DELETE',
   path = '/api/v1/order/all',
   expires = Math.round(new Date().getTime() / 1000) + 6660, // 1 min in the future
-  data = {filter:{"pegPriceType":""},symbol:thepair.replace('BTCUSD','XBTUSD').replace('BTC','U19')};
+  data = {filter:{"ordType":"Limit"},symbol:thepair.replace('BTCUSD','XBTUSD').replace('BTC','U19')};
 
 // Pre-compute the postBody so we can be sure that we're using *exactly* the same body in the request
 // and in the signature. If you don't do this, you might get differently-sorted keys and blow the signature.
