61554081a00e68f401a5fbe077b45a09eaf6e6a4
RegTheIII <jarettrsdunn@gmail.com>
2019-07-16 17:54:17 -0300

    iteration...

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -102,14 +102,7 @@ var requestOptions = {
 request(requestOptions, function(error, response, body) {
   if (error) { console.log(error); }
   for (var j in JSON.parse(body)){
-  var stopQty;
-  if (JSON.parse(body)[j].side == 'Sell'){
-stopQty = JSON.parse(body)[j].orderQty  * -1
-  }
-  else {
-
-stopQty = JSON.parse(body)[j].orderQty
-  }
+  if (stops.includes(JSON.parse(body)[j]['orderID'])){
   if ((stopQty < 0 &&  stopQty < pos ) || (stopQty > 0 && stopQty > pos)){
 console.error('CANCEL STOP')
 stops = stops.splice(JSON.parse(body)[j]['orderID'], 1)
@@ -145,6 +138,7 @@ request(requestOptions, function(error, response, body) {
   })
 
   refreshMargin()
+  }
   }
   }
   })
