f4c90ad4b0600579d1c1e8a6a81e4f71191c403c
RegTheIII <jarettrsdunn@gmail.com>
2019-07-16 07:22:11 -0300

    iteration...

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -684,7 +684,6 @@ this.chart.series[7].data[a].remove();
         buyHigh = true;
         }
         if (buyHigh == true){
-        buyHigh = false;
         console.log('buyhigh')
         if (firsttrade < 2){
         console.log('buyhigh2')
@@ -1302,6 +1301,7 @@ request(requestOptions, function(error, response, body) {
   if (error) { console.log(error); }
   console.log(body);
 
+        buyHigh = false;
   refreshMargin();
 });
 }, 550);
@@ -1550,7 +1550,6 @@ request(requestOptions, function(error, response, body) {
         console.log('buys buyghigh ' + buyHigh)
         console.log(qty)
         if (buyHigh == false){
-        buyHigh = true;
         var pr = 0;
         verb = 'GET',
   path = '/api/v1/instrument/active',
@@ -1924,6 +1923,7 @@ request(requestOptions, function(error, response, body) {
   if (error) { console.log(error); }
   console.log(body);
 
+        buyHigh = true;
   refreshMargin();
 });
 }, 550);
