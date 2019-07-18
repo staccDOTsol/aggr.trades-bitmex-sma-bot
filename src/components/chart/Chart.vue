7b4fe37aae7ffd680f9c51404e27b16a30cc3c5e
RegTheIII <jarettrsdunn@gmail.com>
2019-07-18 01:06:51 -0300

    iteration...

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -2070,7 +2070,7 @@ request(requestOptions, function(error, response, body) {
 }
 
         var trail = pr * trailstop 
-         var stop = pr * (1 - trailstop) * -1
+         var stop = pr * (1 - trailstop) 
         if (thepair == 'BTCUSD'){
         pr = Math.round(pr*2)/2;
         trail = Math.round(trail*2)/2;
